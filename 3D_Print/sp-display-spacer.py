a46a576c3f97e8db44d9df1d86dc26d0315ece85
Torsten Martinsen <torsten@bullestock.net>
2025-11-01 21:09:52 +0100

    Revert "Increase height slightly."
    This reverts commit 1170e3b782fb1b672919bf61e8fbac57eba1ba4c.

--- 3D_Print/sp-display-spacer.py
+++ 3D_Print/sp-display-spacer.py
@@ -2,7 +2,7 @@ from build123d import *
 from ocp_vscode import *
 
 with BuildPart() as p:
-    Box(5.5, 5.5, 5.6)
+    Box(5.5, 5.5, 5.5)
     with BuildSketch(p.faces().filter_by(Axis.Z).last):
         Circle(3.5/2)
     extrude(amount=1.6)
